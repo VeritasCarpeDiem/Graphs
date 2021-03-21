4e3018ca56e5103662131ef7162ce7276971acd7
Jin Kim <Jin.Kim@GMR.local>
2021-03-21 15:50:35 -0700

    finish priorityqueue

--- Heaps/Program.cs
+++ Heaps/Program.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 
 namespace DeleteMeHeaps
 {
+    class Cat { public string Name; }
+
     class Program
     {
         static void Main(string[] args)
         {
+            var comparer = Comparer<Cat>.Default;
+
+
+            ;
+
+            //IComparable comparer;
             Heaps<int> heap = new Heaps<int>(Comparer<int>.Create((a, b) => a.CompareTo(b)));
             heap.Insert(1);
             heap.Insert(9);
@@ -14,7 +22,7 @@ namespace DeleteMeHeaps
             heap.Insert(13);
             heap.Insert(10);
             heap.Insert(3);
-            Heaps<int>.HeapSort(heap, );
+            //Heaps<int>.HeapSort(heap,comparer );
 
         }
     }
